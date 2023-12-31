5ab2964a28b39cff3993b63791bb8fdb8bc5f7f9
trantrungnghia414 <trungnghiadtnt2018@gmail.com>
2023-12-31 08:59:03 +0700

    Cập nhật Source Code

--- CSN/thuchanh/php/admin/index.php
+++ CSN/thuchanh/php/admin/index.php
@@ -51,11 +51,12 @@ $conn->close();
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="../../asess/css/index.css">
-    <title>Form login Admin</title>
+    <link rel="shortcut icon" href="../../asess/img/favicon.ico" type="image/x-icon">
+    <title>Đăng nhập</title>
     <style>
         a.redo {
             padding: 6px 22px;
-            margin-left: 143px;
+            margin-left: 140px;
         }
     </style>
 </head>
